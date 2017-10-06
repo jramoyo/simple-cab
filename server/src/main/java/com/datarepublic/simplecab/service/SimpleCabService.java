8f69e0e08c32439d8d979381530eac7a7c87eaac
Jan Amoyo <jan.amoyo@siteminder.com>
2017-10-06 21:33:51 +1100

    service + test

--- server/src/main/java/com/datarepublic/simplecab/service/SimpleCabService.java
+++ server/src/main/java/com/datarepublic/simplecab/service/SimpleCabService.java
@@ -4,15 +4,49 @@ import com.datarepublic.simplecab.model.MedallionsSummary;
 import com.datarepublic.simplecab.repository.SimpleCabRepository;
 
 import javax.annotation.Resource;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 public final class SimpleCabService {
+
+    private static final String DATE_PATTERN = "YYYY-MM-dd";
+
+    private final ConcurrentMap<String, Integer> cache = new ConcurrentHashMap<>();
 
     @Resource
     private SimpleCabRepository repository;
 
+    public MedallionsSummary getMedallionsSummary(List<String> medallions, Date pickupDate) {
+        return getMedallionsSummary(medallions, pickupDate, false);
+    }
+
     public MedallionsSummary getMedallionsSummary(List<String> medallions, Date pickupDate, boolean ignoreCache) {
-        return null;
+        MedallionsSummary summary = new MedallionsSummary();
+
+        for (String medallion : medallions) {
+            summary.addTrip(medallion, getCount(medallion, pickupDate, ignoreCache));
+        }
+
+        return summary;
+    }
+
+    private Integer getCount(String medallion, Date pickupDate, boolean ignoreCache) {
+        if (ignoreCache) {
+            return repository.getCountByMedallionAndPickupDatetime(medallion, pickupDate);
+        }
+
+        String key = medallion + "|" + toString(pickupDate);
+        return cache.computeIfAbsent(key, _key -> repository.getCountByMedallionAndPickupDatetime(medallion, pickupDate));
+    }
+
+    private String toString(Date pickupDate) {
+        return new SimpleDateFormat(DATE_PATTERN).format(pickupDate);
+    }
+
+    public void invalidateCache() {
+        cache.clear();
     }
 }
--- server/src/test/java/com/datarepublic/simplecab/service/SimpleCabServiceTest.java
+++ server/src/test/java/com/datarepublic/simplecab/service/SimpleCabServiceTest.java
@@ -1,13 +1,19 @@
 package com.datarepublic.simplecab.service;
 
+import com.datarepublic.simplecab.model.MedallionsSummary;
 import com.datarepublic.simplecab.repository.SimpleCabRepository;
+import com.google.common.collect.Lists;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import static org.mockito.Mockito.when;
+import java.util.Date;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class SimpleCabServiceTest {
@@ -19,7 +25,80 @@ public class SimpleCabServiceTest {
     private SimpleCabService service;
 
     @Test
-    public void it_does_something() {
-        when(repository.getCountByMedallionAndPickupDatetime(null, null)).thenReturn(1);
+    public void it_fetches_from_the_repository_then_fetches_from_the_cache_afterwards() {
+        String medallion1 = "medallion1";
+        String medallion2 = "medallion2";
+        String medallion3 = "medallion3";
+        Date pickupDate = new Date();
+
+        when(repository.getCountByMedallionAndPickupDatetime(medallion1, pickupDate)).thenReturn(1);
+        when(repository.getCountByMedallionAndPickupDatetime(medallion2, pickupDate)).thenReturn(2);
+        when(repository.getCountByMedallionAndPickupDatetime(medallion3, pickupDate)).thenReturn(3);
+
+        MedallionsSummary summary = service.getMedallionsSummary(Lists.newArrayList(medallion1, medallion2, medallion3), pickupDate);
+
+        assertThat(summary.getTrips().get("medallion1"), is(1));
+        assertThat(summary.getTrips().get("medallion2"), is(2));
+        assertThat(summary.getTrips().get("medallion3"), is(3));
+
+        service.getMedallionsSummary(Lists.newArrayList(medallion1, medallion2, medallion3), pickupDate);
+        service.getMedallionsSummary(Lists.newArrayList(medallion1, medallion2, medallion3), pickupDate);
+        service.getMedallionsSummary(Lists.newArrayList(medallion1, medallion2, medallion3), pickupDate);
+
+        verify(repository, times(1)).getCountByMedallionAndPickupDatetime(medallion1, pickupDate);
+        verify(repository, times(1)).getCountByMedallionAndPickupDatetime(medallion2, pickupDate);
+        verify(repository, times(1)).getCountByMedallionAndPickupDatetime(medallion3, pickupDate);
+    }
+
+    @Test
+    public void it_always_fetches_from_the_repository_if_ignoreCache_is_true() {
+        String medallion1 = "medallion1";
+        String medallion2 = "medallion2";
+        String medallion3 = "medallion3";
+        Date pickupDate = new Date();
+
+        when(repository.getCountByMedallionAndPickupDatetime(medallion1, pickupDate)).thenReturn(1);
+        when(repository.getCountByMedallionAndPickupDatetime(medallion2, pickupDate)).thenReturn(2);
+        when(repository.getCountByMedallionAndPickupDatetime(medallion3, pickupDate)).thenReturn(3);
+
+        MedallionsSummary summary = service.getMedallionsSummary(Lists.newArrayList(medallion1, medallion2, medallion3), pickupDate);
+
+        assertThat(summary.getTrips().get("medallion1"), is(1));
+        assertThat(summary.getTrips().get("medallion2"), is(2));
+        assertThat(summary.getTrips().get("medallion3"), is(3));
+
+        service.getMedallionsSummary(Lists.newArrayList(medallion1, medallion2, medallion3), pickupDate, true);
+        service.getMedallionsSummary(Lists.newArrayList(medallion1, medallion2, medallion3), pickupDate, true);
+        service.getMedallionsSummary(Lists.newArrayList(medallion1, medallion2, medallion3), pickupDate, true);
+
+        verify(repository, times(4)).getCountByMedallionAndPickupDatetime(medallion1, pickupDate);
+        verify(repository, times(4)).getCountByMedallionAndPickupDatetime(medallion2, pickupDate);
+        verify(repository, times(4)).getCountByMedallionAndPickupDatetime(medallion3, pickupDate);
+    }
+
+    @Test
+    public void it_allows_the_cache_to_be_invalidated() {
+        String medallion1 = "medallion1";
+        String medallion2 = "medallion2";
+        String medallion3 = "medallion3";
+        Date pickupDate = new Date();
+
+        when(repository.getCountByMedallionAndPickupDatetime(medallion1, pickupDate)).thenReturn(1);
+        when(repository.getCountByMedallionAndPickupDatetime(medallion2, pickupDate)).thenReturn(2);
+        when(repository.getCountByMedallionAndPickupDatetime(medallion3, pickupDate)).thenReturn(3);
+
+        MedallionsSummary summary = service.getMedallionsSummary(Lists.newArrayList(medallion1, medallion2, medallion3), pickupDate);
+
+        assertThat(summary.getTrips().get("medallion1"), is(1));
+        assertThat(summary.getTrips().get("medallion2"), is(2));
+        assertThat(summary.getTrips().get("medallion3"), is(3));
+
+        service.invalidateCache();
+
+        service.getMedallionsSummary(Lists.newArrayList(medallion1, medallion2, medallion3), pickupDate);
+
+        verify(repository, times(2)).getCountByMedallionAndPickupDatetime(medallion1, pickupDate);
+        verify(repository, times(2)).getCountByMedallionAndPickupDatetime(medallion2, pickupDate);
+        verify(repository, times(2)).getCountByMedallionAndPickupDatetime(medallion3, pickupDate);
     }
 }
